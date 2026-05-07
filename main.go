ea5540686607cec524a0e8f4918bca44ca72eb5f
Adam Lear <adamlear12@gmail.com>
2026-05-06 22:45:51 -0500

    feat: add basic example to fetch repositories

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/google/go-github/v85/github"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
-	api := fiber.New()
-	api.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	githubToken := os.Getenv("GITHUB_TOKEN")
+	githubClient := github.NewClient(nil).WithAuthToken(githubToken)
 
 	app := fiber.New()
-	app.Use("/api", api)
+
+	api := app.Group("/api")
+	api.Get("/repos", func(c fiber.Ctx) error {
+		repos, _, err := githubClient.Repositories.ListAll(c, nil)
+		if err != nil {
+			fiber.NewError(fiber.StatusInternalServerError, "could not fetch reps")
+		}
+		return c.JSON(repos)
+	})
+
 	app.Listen(":3000")
 }
