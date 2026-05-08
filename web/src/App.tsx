6d9eb46d73006a6ce3ee655f8365bdec2a2b8979
Adam Lear <adamlear12@gmail.com>
2026-05-07 23:28:05 -0500

    fix: build issues after creating project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,8 @@
-import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
+    <Button></Button>
   )
 }
 
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,9 +2,15 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
